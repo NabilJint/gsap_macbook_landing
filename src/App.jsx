d124e2276c4ec7e6d05673ae88330afeb0f18c23
Nabil Jint <yusufjintoro@gmail.com>
2025-12-12 21:26:25 +0100

    Implement Performance Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ProductViewer from "./Components/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Showcase from "./Components/Showcase";
+import Performance from "./Components/Performance";
+import Features from "./Components/Features";
+import Highlights from "./Components/Highlights";
+import Footer from "./Components/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 // Prevent GSAP from trying to fix 100vh for mobile
@@ -17,6 +21,10 @@ const App = () => {
       <Hero />
       <ProductViewer />
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   );
 };
